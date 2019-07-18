e7c0c981899c7aa8c7716ffeacd1af2fffe62423
Jordan Randleman <jrandleman@scu.edu>
2019-07-17 22:59:11 -0700

    Now parses links imbedded in the page's JavaScript

--- parseLinks.js
+++ parseLinks.js
@@ -1,4 +1,4 @@
-// Author: Jordan Randleman - HTML Link Parsing Algorithm.
+// Author: Jordan Randleman - Webpage Link Parsing Algorithm.
 
 /* 
  * Given an HTML page & its URL, returns a URL object of HTML's sorted
@@ -52,22 +52,25 @@ exports.Parser = (html, siteUrl) => {
   let urls = getSiteUrlExtensions(siteUrl);
   // W/o last filepath,  with last filepath.
   let rootUrl = urls[0], lastFileUrl = urls[1];
-  let linkPrefixes = ['src="', 'SRC="', 'href="', 'HREF="'];
+  let linkPrefixes = ['src="', 'SRC="', 'href="', 'HREF="', 'http'];
   var links = {};
   for(let prefix of linkPrefixes) { // For each link prefix.
-    let start = 0, prefixSize = prefix.length;
+    let start = 0, prefixSize = (prefix === 'http') ? 1 : prefix.length;
     while(true) { // While still links with prefix to scraped.
       start = html.indexOf(prefix, start) + prefixSize;
       if(start - prefixSize === -1) break; // No more links with prefix.
-      if(html[start] == '"' || html[start+1] == '"') continue; // Empty url.
-      let linkType = getLinkHtmlType(start, html);
-      let link = html.slice(start, html.indexOf('"', start));
+      if(invalidLink(html, start, prefix)) continue;
+      let linkType = (prefix === 'http') ? null : getLinkHtmlType(start, html);
+      let endOfLink = getEndOfLinkIndex(html, start);
+      let link = html.slice(start, endOfLink).replace(/\\\//g, '/');
+      if(prefix === 'http') link = decodedLink(html[start - 1] + link);
       if(link.slice(0,2) == '//') link = 'https:' + link;
       if(link.slice(0, 4) != 'http') { // Extends current url.
         let header = (link[0] == '/') ? rootUrl : (link[0] == '#') ? siteUrl : lastFileUrl;
         link = header + ((header[header.length-1] != '/' && link[0] != '#') ? '/' + link : link);
       }
       link = formattedLink(link);
+      if(!link.includes('//')) continue;
       if(!linkType) { // Couldn't find tag/rel/type for link.
         let linkExtension = getLinkExtension(link);
         linkType = getLinkExtensionType(siteUrl, link, linkExtension);
@@ -86,15 +89,11 @@ exports.Parser = (html, siteUrl) => {
 
 // Returns base/full site urls w/o & w/ last filename extension.
 function getSiteUrlExtensions(siteUrl) {
-  let firstSlashIdx = siteUrl.indexOf('/', 9); // After 'https://'
-  var pageIdx = siteUrl.lastIndexOf('/'), lastFileIdx = siteUrl.length;
-  if(pageIdx != 6 && pageIdx != 7 && pageIdx != -1) { // Not 'http://' nor 'https://'
-    lastFileIdx = siteUrl.lastIndexOf('/', pageIdx - 1);
-    var lastFileName = (lastFileIdx != -1) ? siteUrl.slice(lastFileIdx, pageIdx + 1) : '';
-  }
-  var baseUrl = (lastFileIdx != -1) ? siteUrl.slice(0, lastFileIdx) : siteUrl;
+  let firstSlashIdx = siteUrl.indexOf('/', 9); // After 'https://'.
+  let lastSlashIdx = siteUrl.lastIndexOf('/') + 1;
+  var lastFileUrl = (lastSlashIdx > 6) ? siteUrl.slice(0, lastSlashIdx) : siteUrl;
   var rootUrl = (firstSlashIdx != -1) ? siteUrl.slice(0, firstSlashIdx) : siteUrl;
-  return [rootUrl, baseUrl + lastFileName];
+  return [rootUrl, lastFileUrl];
 }
 
 // Returns a link's file extension.
@@ -127,31 +126,24 @@ function getLinkExtensionType(siteUrl, link, extension) {
 
 // Returns link's type based off of tag/rel/type, if exists.
 function getLinkHtmlType(start, html) {
-  var linkTag = getLinkTag(start, html);
-  if(linkTag) return linkTag;
-  var relAttrib = getLinkAttribute(start, html, 'rel="', 'stylesheet', 'shortcut icon');
+  let tags = [start, html, '<', 'style', 'img'];
+  let rels = [start, html, 'rel="', 'stylesheet', 'shortcut icon'];
+  let types = [start, html, 'type="', 'text/css', 'image/x-icon'];
+  var tagAttrib = getLinkAttrib(...tags,(str,e1,e2)=>str.lastIndexOf(e1,e2),(n1,n2)=>n1>n2);
+  if(tagAttrib) return tagAttrib;
+  var relAttrib = getLinkAttrib(...rels,(str,e1,e2)=>str.indexOf(e1,e2),(n1,n2)=>n1<n2);
   if(relAttrib) return relAttrib;
-  var typeAttrib = getLinkAttribute(start, html, 'type="', 'text/css', 'image/x-icon');
+  var typeAttrib = getLinkAttrib(...types,(str,e1,e2)=>str.indexOf(e1,e2),(n1,n2)=>n1<n2);
   return typeAttrib;
 }
 
-// Returns link's <tag>.
-function getLinkTag(start, html) {
-  let tagStartIdx = html.lastIndexOf('<', start) + 1;
-  let tagEndIdx = html.lastIndexOf('>', start) + 1;
-  if(tagStartIdx != 0 && tagStartIdx > tagEndIdx) {
-    let tagPrefix = html.slice(tagStartIdx, html.indexOf(' ', tagStartIdx));
-    if(tagPrefix == 'script' || tagPrefix == 'img') return tagPrefix;
-  }
-  return null;
-}
-
-// Returns link's 'rel' or 'type' attribute.
-function getLinkAttribute(start, html, attrib, css, img) {
-  let attribIdx = html.indexOf(attrib, start) + attrib.length;
-  let tagEndIdx = html.indexOf('>', start);
-  if(attribIdx != attrib.length - 1 && attribIdx < tagEndIdx) {
-    let attribStr = html.slice(attribIdx, 14);
+// Returns link's <tag>, 'rel', or 'type' attribute.
+function getLinkAttrib(start, html, attrib, css, img, indexFcn, validAttrib) {
+  let attribIdx = indexFcn(html, attrib, start) + attrib.length;
+  let tagEndIdx = indexFcn(html, '>', start);
+  if(attribIdx != attrib.length - 1 && validAttrib(attribIdx, tagEndIdx)) {
+    let endOfAttrib = (attrib === '<') ? html.indexOf(' ', attribIdx) : 14;
+    let attribStr = html.slice(attribIdx, endOfAttrib);
     if(attribStr.includes(css)) return 'style';
     if(attribStr.includes(img)) return 'img';
   }
@@ -165,6 +157,30 @@ function formattedLink(link) {
   let linkTail = link.slice(headIdx).replace(/\/\//g, '/')
                      .replace(/&amp;/g, '&').replace('javascript:void(0);', '');
   return httpHead + linkTail;
+}
+
+// Returns the index of the end of the link at the 'start' index.
+function getEndOfLinkIndex(html, start) {
+  let linkEndings = ['"', ' ', '&quot;', '<'];
+  let idxs = linkEndings.map(e => html.indexOf(e, start)).filter(e => e >= 0);
+  return Math.min(...idxs);
+}
+
+// Returns a decoded-uri-ified link.
+function decodedLink(link) {
+  if(link && link[4] && 's:%'.indexOf(link[4]) !== -1) {
+    try { link = decodeURIComponent(link).replace(/\\\//g, '/'); } catch(err) {}
+  }
+  return link;
+}
+
+// Returns true if link's non-nested but searching for nested (in js) links.
+function invalidLink(html, start, prefix) {
+  if(prefix === 'http') {
+    let linkPrefix = html.slice(start - 7, start - 1).toLowerCase();
+    if(linkPrefix.includes('href="') || linkPrefix.includes('src="')) return true;
+  }
+  return (html[start] == '"' || html[start+1] == '"'); // If link is empty.
 }
 
 /******************************************************************************/
